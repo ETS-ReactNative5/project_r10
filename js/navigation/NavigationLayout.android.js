17109ac0ab249c464f9b0a0ddd5ce9d48de8fd83
Kanika Wadhawan <wadhawan.kanika@gmail.com>
2019-11-21 13:20:46 -0800

    add different navigation layout for android and ios

---
+++ js/navigation/NavigationLayout.android.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+import {createStackNavigator} from 'react-navigation-stack';
+import AboutScreen from '../screens/About';
+import FavesScreen from '../screens/Faves';
+import MapScreen from '../screens/Map';
+import ScheduleScreen from '../screens/Schedule';
+import SessionScreen from '../screens/Session';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {colors, typography} from '../config/styles';
+import {grey} from 'ansi-colors';
+import {sharedNavigationOptions} from './config';
+
+const {black, mediumGrey, white} = colors;
+
+const AboutStack = createStackNavigator(
+  {
+    About: AboutScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+      initialRouteName: 'About',
+      title: 'About',
+    }),
+  },
+);
+
+const FavesStack = createStackNavigator(
+  {
+    Faves: FavesScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+      initialRouteName: 'Faves',
+      title: 'Faves',
+    }),
+  },
+);
+
+const MapStack = createStackNavigator(
+  {
+    Maps: MapScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+      initialRouteName: 'Maps',
+      title: 'Maps',
+    }),
+  },
+);
+
+const ScheduleStack = createStackNavigator(
+  {
+    Schedule: ScheduleScreen,
+    Session: SessionScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+      initialRouteName: 'Schedule',
+      title: 'Schedule',
+    }),
+  },
+);
+
+MapStack.navigationOptions = {
+  drawerIcon: ({tintColor}) => (
+    <Icon name="md-map" size={25} color={tintColor} />
+  ),
+};
+ScheduleStack.navigationOptions = {
+  drawerIcon: ({tintColor}) => (
+    <Icon name="md-calendar" size={25} color={tintColor} />
+  ),
+};
+FavesStack.navigationOptions = {
+  drawerIcon: ({tintColor}) => (
+    <Icon name="md-heart" size={25} color={tintColor} />
+  ),
+};
+AboutStack.navigationOptions = {
+  drawerIcon: ({tintColor}) => (
+    <Icon name="md-information-circle" size={25} color={tintColor} />
+  ),
+};
+
+// Dedicated stacks for Schedule, Map and Faves will go here too!
+export default createDrawerNavigator(
+  {
+    About: AboutStack,
+    Faves: FavesStack,
+    Maps: MapStack,
+    Schedule: ScheduleStack,
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: white,
+      inactiveTintColor: mediumGrey,
+      tabStyle: {
+        backgroundColor: black,
+      },
+      labelStyle: {
+        fontSize: 10,
+        fontFamily: typography.fontMain,
+      },
+    },
+  },
+);
---
+++ js/navigation/NavigationLayout.ios.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createStackNavigator} from 'react-navigation-stack';
+import AboutScreen from '../screens/About';
+import FavesScreen from '../screens/Faves';
+import MapScreen from '../screens/Map';
+import ScheduleScreen from '../screens/Schedule';
+import SessionScreen from '../screens/Session';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {colors, typography} from '../config/styles';
+import {grey} from 'ansi-colors';
+import {sharedNavigationOptions} from './config';
+
+const {black, mediumGrey, white} = colors;
+
+const AboutStack = createStackNavigator(
+  {
+    About: AboutScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+      initialRouteName: 'About',
+      title: 'About',
+    }),
+  },
+);
+
+const FavesStack = createStackNavigator(
+  {
+    Faves: FavesScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+      initialRouteName: 'Faves',
+      title: 'Faves',
+    }),
+  },
+);
+
+const MapStack = createStackNavigator(
+  {
+    Maps: MapScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+      initialRouteName: 'Maps',
+      title: 'Maps',
+    }),
+  },
+);
+
+const ScheduleStack = createStackNavigator(
+  {
+    Schedule: ScheduleScreen,
+    Session: SessionScreen,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      ...sharedNavigationOptions(navigation),
+      initialRouteName: 'Schedule',
+      title: 'Schedule',
+    }),
+  },
+);
+
+// Dedicated stacks for Schedule, Map and Faves will go here too!
+export default createBottomTabNavigator(
+  {
+    About: AboutStack,
+    Faves: FavesStack,
+    Maps: MapStack,
+    Schedule: ScheduleStack,
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, tintColor}) => {
+        const {routeName} = navigation.state;
+        let iconName;
+        if (routeName === 'Schedule') {
+          iconName = 'ios-calendar';
+        } else if (routeName === 'Maps') {
+          iconName = 'ios-map';
+        } else if (routeName === 'About') {
+          iconName = 'ios-information-circle';
+        } else if (routeName === 'Faves') {
+          iconName = 'ios-heart';
+        }
+        return <Ionicons name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: white,
+      inactiveTintColor: mediumGrey,
+      tabStyle: {
+        backgroundColor: black,
+      },
+      labelStyle: {
+        fontSize: 10,
+        fontFamily: typography.fontMain,
+      },
+    },
+  },
+);
